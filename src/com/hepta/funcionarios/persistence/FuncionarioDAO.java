99b39f100396ec6e96f1a5203cef13f8bf80e2f7
billybrian <billybrianm@hotmail.com>
2021-02-03 23:10:15 -0300

    Adicionado get de setores para o cadastro de usuários

--- src/com/hepta/funcionarios/persistence/FuncionarioDAO.java
+++ src/com/hepta/funcionarios/persistence/FuncionarioDAO.java
@@ -7,14 +7,15 @@ import javax.persistence.EntityManager;
 import javax.persistence.Query;
 
 import com.hepta.funcionarios.entity.Funcionario;
+import com.hepta.funcionarios.entity.Setor;
 
 public class FuncionarioDAO {
 
-	public void save(Funcionario Funcionario) throws Exception {
+	public void save(Funcionario funcionario) throws Exception {
 		EntityManager em = HibernateUtil.getEntityManager();
 		try {
 			em.getTransaction().begin();
-			em.persist(Funcionario);
+			em.persist(funcionario);
 			em.getTransaction().commit();
 		} catch (Exception e) {
 			em.getTransaction().rollback();
@@ -84,6 +85,22 @@ public class FuncionarioDAO {
 			em.close();
 		}
 		return Funcionarios;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<Setor> getAllSetores() throws Exception {
+		EntityManager em = HibernateUtil.getEntityManager();
+		List<Setor> Setores = new ArrayList<>();
+		try {
+			Query query = em.createQuery("FROM Setor");
+			Setores = query.getResultList();
+		} catch (Exception e) {
+			em.getTransaction().rollback();
+			throw new Exception(e);
+		} finally {
+			em.close();
+		}
+		return Setores;
 	}
 
 }
